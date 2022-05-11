6b4b404a4f472d4a3045cc5d8500111523bef964
pg <g_poonam@hotmail.com>
2022-05-11 15:19:33 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
    
-       Hello World!
+       Hello World amplify!
         </p>
       
       </header>
